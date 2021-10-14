6a7dc17c132faee281d41949ccba152496ff4901
carlosandrescardenas <andres2158@hotmail.com>
2021-10-14 10:53:58 -0500

    receive the backendHost from environment variables

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Declare our dependencies
 var express = require('express');
 var request = require('superagent');
-var backendHost = process.env.BACK_HOST || '192.168.55.6';
+var backendHost = process.env.BACK_HOST;
 // Create our express app
 var app = express();
 
