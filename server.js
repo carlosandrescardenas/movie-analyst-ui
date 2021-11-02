f87d63668e5b7dfa48f7ea6e3f0deaee57f8ea0d
carlosandrescardenas <andres2158@hotmail.com>
2021-11-02 16:11:48 -0500

    update back host

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Declare our dependencies
 var express = require('express');
 var request = require('superagent');
-var backendHost = process.env.BACK_HOST || '10.1.88.237';
+var backendHost = process.env.BACK_HOST || '10.1.80.4';
 // Create our express app
 var app = express();
 
